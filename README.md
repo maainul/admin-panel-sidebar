1a7c9927c29ac90021c847ffed59dd89b87de828
Md.Mainul Hasan <mdevhasan@gmail.com>
2024-06-11 10:52:30 +0600

    Complete Sidebar

---
+++ README.md
@@ -0,0 +1,6 @@
+Link : https://www.youtube.com/watch?v=fI1Bj-X7MuE&ab_channel=%D9%83%D9%88%D8%B1%D8%B3%D8%A7%D8%AA%D9%81%D9%8A%D8%A7%D9%84%D8%A8%D8%B1%D9%85%D8%AC%D8%A9-KorsatXParmaga
+
+![image](https://github.com/maainul/FullStackThings/assets/37740006/15ce48e2-a772-40d9-a2a3-79ed11cc56a6)
+![image](https://github.com/maainul/FullStackThings/assets/37740006/9306edbe-cb71-4b8b-badd-2341dd127f0e)
+
+
---
+++ script.js
@@ -0,0 +1,27 @@
+const listItems = document.querySelectorAll(".sidebar-list li")
+
+listItems.forEach(item => {
+    item.addEventListener("click", () => {
+        let isActive = item.classList.contains("active")
+
+        listItems.forEach(el => {
+            el.classList.remove("active")
+        })
+        if (isActive) item.classList.remove("active")
+        else item.classList.add("active")
+
+    })
+})
+
+
+const toggleSidebar = document.querySelector(".toggle-sidebar")
+const sidebar = document.querySelector(".sidebar")
+const logo = document.querySelector(".logo-box")
+
+toggleSidebar.addEventListener("click", () => {
+    sidebar.classList.toggle("close")
+})
+
+logo.addEventListener("click", () => {
+    sidebar.classList.toggle("close")
+})
